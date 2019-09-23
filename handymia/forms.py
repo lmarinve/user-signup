67aaa1e7a680d2e58bb5ea75fd5d65d3d28798b4
Ubuntu <ubuntu@ip-172-31-0-202.us-east-2.compute.internal>
2019-09-23 02:13:51 +0000

    login validation added

--- handymia/forms.py
+++ handymia/forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    username = StringField('Username', [validators.DataRequired(), validators.Length(min=3,
+                                        message='Little short'), Length(max=20, message=u'too long'),
+                                        validators.Regexp('^[A-Za-z]*$', message='Must have only letters')])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    email = StringField('Email Address', [validators.Length(min=3, message=u'Little short'), Length(max=20,
+                                          message=u'too long'), Email(message='not a valid email address')])
     submit = SubmitField('Sign In')
--- handymia/routes.py
+++ handymia/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from handymia import app
 from handymia.forms import LoginForm
 
@@ -13,22 +13,8 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign in', form=form)
-'''
-from flask import render_template
-from handymia import app
-#from handymia.handymia import LoginForm
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    welcome = {'message': 'Welcome to HandyMia'}
-    return render_template("main.html", title='Home', welcome=welcome)
-
-
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign in', form=form)
-'''
